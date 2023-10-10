4c409a72e59752028d4d59d8ddd8d02bf5b6c1ec
juni <woajun@naver.com>
2023-10-10 17:54:54 +0900

    add handleMapClick

--- app/KakaoMap.tsx
+++ app/KakaoMap.tsx
@@ -1,31 +1,56 @@
-"use client";
+'use client';
 
-import Script from "next/script";
-import { useEffect, useRef, useState } from "react";
+import Script from 'next/script';
+import { useEffect, useRef, useState, useCallback } from 'react';
 
-const KakaoMap = () => {
+type Props = {
+  handleMapClick?: (e: KakaoMapClickEvent, map: kakao.maps.Map) => void;
+};
+
+const KakaoMap = ({ handleMapClick = () => {} }: Props) => {
   const palette = useRef<HTMLDivElement | null>(null);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [map, setMap] = useState<kakao.maps.Map | null>(null);
 
   useEffect(() => {
     if (isLoaded && palette.current) {
-      new daum.maps.Map(palette.current, {
-        center: new daum.maps.LatLng(37.543341, 127.052727),
+      const { Map, LatLng } = kakao.maps;
+      const aMap = new Map(palette.current, {
+        center: new LatLng(37.543341, 127.052727),
         level: 4,
       });
+
+      setMap(aMap);
     }
   }, [isLoaded]);
+
+  const clickEvent = useRef<(e: KakaoMapClickEvent) => void>(() => {});
+  const mapClickHandler = useCallback((e: KakaoMapClickEvent) => {
+    if (map) {
+      handleMapClick(e, map);
+    }
+  }, [handleMapClick, map])
+
+  useEffect(() => {
+    if (map) {
+      const { event } = kakao.maps;
+      event.removeListener(map, 'click', clickEvent.current);
+      event.addListener(map, 'click', mapClickHandler);
+      clickEvent.current = mapClickHandler;
+    }
+  }, [mapClickHandler, map]);
+
   return (
     <>
       <Script
         src="//dapi.kakao.com/v2/maps/sdk.js?appkey=105b185094e8d46221227fd3ecff6497&autoload=false"
         onReady={() => {
-          daum.maps.load(() => {
+          kakao.maps.load(() => {
             setIsLoaded(true);
           });
         }}
       />
-      <div ref={palette} className="w-full" style={{ height: "600px" }}></div>
+      <div ref={palette} className="w-full" style={{ height: '600px' }}></div>
     </>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+"use client"
+
 import KakaoMap from "./KakaoMap";
+import { useState } from 'react';
 
 export default function Home() {
+  const handleMapClick = (e: KakaoMapClickEvent) => {
+    console.log("hellow");
+  }
+  const handleMapClick2 = (e: KakaoMapClickEvent) => {
+    console.log("안녕");
+  }
+
+  const [apple, setApple] = useState(false); 
   return (
     <>
+    <button onClick={() => setApple(!apple)}>
+      event change
+    </button>
       <h1>성수 팝업</h1>
-      <KakaoMap></KakaoMap>
+      <KakaoMap handleMapClick={apple?handleMapClick: handleMapClick2}></KakaoMap>
     </>
   );
 }
