cd98ad32ace25af3fac0f833054b930d7b6a8c82
juni <woajun@naver.com>
2023-10-11 16:43:01 +0900

    add bottom arrow

--- app/page.tsx
+++ app/page.tsx
@@ -9,18 +9,25 @@ function Overlay(position: kakao.maps.LatLng) {
   };
 
   return (
-    <div onMouseDown={handleClick} className="bg-white border-2 rounded-xl absolute bottom-0 translate-x-[-50%] px-3 py-2 z-50 cursor-auto">
-      <h1 className='font-bold text-center'>현위치</h1>
-      <div className='pt-2'>
-        <div className="font-bold w-30">위도(latitude)</div>
-        <span>{position.getLat()}</span>
-      </div>
-      <div>
-        <div className="font-bold w-30">경도(longitude)</div>
-        <span>{position.getLng()}</span>
-      </div>
-      <div className="flex justify-center pt-2">
-        <button className="px-4 py-1 font-bold rounded-md bg-amber-300 hover:bg-amber-400 active:bg-amber-300">복사</button>
+    <div className="arrow absolute bottom-0 translate-x-[-50%] z-50 pb-[9px]">
+      <div
+        className="px-3 py-2 bg-white border-2 cursor-auto rounded-xl"
+        onMouseDown={handleClick}
+      >
+        <h1 className="font-bold text-center">현위치</h1>
+        <div className="pt-2">
+          <div className="font-bold w-30">위도(latitude)</div>
+          <span>{position.getLat()}</span>
+        </div>
+        <div>
+          <div className="font-bold w-30">경도(longitude)</div>
+          <span>{position.getLng()}</span>
+        </div>
+        <div className="flex justify-center pt-2">
+          <button className="px-4 py-1 font-bold rounded-md bg-amber-300 hover:bg-amber-400 active:bg-amber-300">
+            복사
+          </button>
+        </div>
       </div>
     </div>
   );
