df594209249e2ab98fc4969ff022291ad78f3b7b
juni <woajun@naver.com>
2023-10-12 17:58:00 +0900

    add pagination ui

---
+++ app/places/Pagination.tsx
@@ -0,0 +1,109 @@
+type Props = {
+  currentPage: number;
+  totalPage: number;
+  handlePageClick: (n: number) => void;
+  handlePrevClick: () => void;
+  handleNextClick: () => void;
+};
+
+export default function Pagination({
+  currentPage,
+  totalPage,
+  handlePageClick,
+  handlePrevClick,
+  handleNextClick,
+}: Props) {
+  return (
+    <nav aria-label="Page navigation example">
+      <ul className="flex items-center -space-x-px h-8 text-sm">
+        <li>
+          <a
+            href="#"
+            className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700"
+          >
+            <span className="sr-only">Previous</span>
+            <svg
+              className="w-2.5 h-2.5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M5 1 1 5l4 4"
+              />
+            </svg>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+          >
+            1
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+          >
+            2
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            aria-current="page"
+            className="z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700   "
+          >
+            3
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+          >
+            4
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+          >
+            5
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700"
+          >
+            <span className="sr-only">Next</span>
+            <svg
+              className="w-2.5 h-2.5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="m1 9 4-4-4-4"
+              />
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- app/places/PlaceList.tsx
+++ app/places/PlaceList.tsx
@@ -5,6 +5,7 @@ import {
   usePlaceSearchListDispatch,
 } from '../stores/PlaceSearchList.tsx/PlaceSearchListContext';
 import { useMouseOverPlaceDispatch } from '../stores/MouseOverPlace/MouseOverPlaceContext';
+import Pagination from './Pagination';
 
 export default function PlaceList() {
   const isLoaded = useIsLoadedMap();
@@ -33,32 +34,45 @@ export default function PlaceList() {
       }
     });
   }, [keyword, ps, pListDispatch]);
+
+  console.log(pList);
   return (
-    <div>
-      <input
-        type="text"
-        onChange={(e) => {
-          setKeyword(e.target.value);
-        }}
-      />
-      {pList &&
-        pList.data.map((e) => {
-          return (
-            <div
-              key={e.id}
-              onMouseOver={() => {
-                if (!moPlaceDispatch) return;
-                moPlaceDispatch({ type: 'set', payload: { place: e } });
-              }}
-              onMouseLeave={() => {
-                if (!moPlaceDispatch) return;
-                moPlaceDispatch({ type: 'clear' });
-              }}
-            >
-              {e.place_name}
-            </div>
-          );
-        })}
+    <div className="py-3 px-2 flex flex-col justify-between h-full">
+      <div>
+        <input
+          type="text"
+          className="w-full border rounded-md px-2 py-2"
+          onChange={(e) => {
+            setKeyword(e.target.value);
+          }}
+        />
+        <div className="flex flex-col gap-1 p-2">
+          <div className="text-end">
+            검색결과: {pList?.pagination.totalCount ?? 0} 건
+          </div>
+          {pList &&
+            pList.data.map((e) => {
+              return (
+                <div
+                  key={e.id}
+                  onMouseOver={() => {
+                    if (!moPlaceDispatch) return;
+                    moPlaceDispatch({ type: 'set', payload: { place: e } });
+                  }}
+                  onMouseLeave={() => {
+                    if (!moPlaceDispatch) return;
+                    moPlaceDispatch({ type: 'clear' });
+                  }}
+                >
+                  {e.place_name}
+                </div>
+              );
+            })}
+        </div>
+      </div>
+      <div className="flex justify-center pb-5">
+        <Pagination p={pList?.pagination} />
+      </div>
     </div>
   );
 }
