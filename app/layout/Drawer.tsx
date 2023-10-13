1d84a46f1a964b4ee2aa744b57953e9e69b67019
juni <woajun@naver.com>
2023-10-13 14:25:02 +0900

    fix clickable bug

--- app/layout/Drawer.tsx
+++ app/layout/Drawer.tsx
@@ -8,17 +8,17 @@ type Props = {
 
 export default function Drawer({ drawer, children }: Props) {
   return (
-    <div className={`${drawer ? 'z-10' : ''} w-full h-full absolute flex `}>
+    <div className={`z-10 w-full h-full absolute flex pointer-events-none`}>
       <div
         className={`${
           !drawer && '-translate-x-full'
-        } w-80 bg-orange-50 transform transition-transform duration-300 ease-in-out z-20 lg:translate-x-0`}
+        } w-80 bg-orange-50 transform transition-transform duration-300 ease-in-out z-20 lg:translate-x-0 pointer-events-auto`}
       >
         {children}
       </div>
       <Link
         href="?drawer=false"
-        className="absolute w-full h-full bg-black grow opacity-30 lg:hidden"
+        className={`absolute w-full h-full bg-black grow opacity-30 lg:hidden pointer-events-auto ${!drawer && 'hidden'}`}
       ></Link>
     </div>
   );
