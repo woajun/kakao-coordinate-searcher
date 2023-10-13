1be148f48e3a08eacb2b31603e8095d9da85960f
juni <woajun@naver.com>
2023-10-13 14:53:00 +0900

    use PageButton

--- app/places/Pagination.tsx
+++ app/places/Pagination.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react';
+
 function LeftArrowSvg() {
   return (
     <svg
@@ -39,16 +41,17 @@ function RightArrowSvg() {
 }
 
 type PageButtonProps = {
-  page: number;
+  children: ReactNode;
   state?: 'active' | 'disable' | 'normal';
+  className?: string;
 };
 
-function PageButton({ page, state = 'normal' }: PageButtonProps) {
+function PageButton({ children, state = 'normal', className }: PageButtonProps) {
   return (
     <li>
       <a
         href="#"
-        className={`
+        className={`${className} 
         ${
           state === 'active' &&
           'z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700'
@@ -63,7 +66,7 @@ function PageButton({ page, state = 'normal' }: PageButtonProps) {
         }
         `}
       >
-        {page}
+        {children}
       </a>
     </li>
   );
@@ -86,30 +89,20 @@ export default function Pagination({
 }: Props) {
   return (
     <nav aria-label="Page navigation example">
-      <ul className="flex items-center -space-x-px h-8 text-sm">
-        <li>
-          <a
-            href="#"
-            className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700"
-          >
-            <span className="sr-only">Previous</span>
-            <LeftArrowSvg />
-          </a>
-        </li>
-        <PageButton page={1} />
-        <PageButton page={2} />
-        <PageButton page={3} state="active" />
-        <PageButton page={4} />
-        <PageButton page={5} state="disable" />
-        <li>
-          <a
-            href="#"
-            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700"
-          >
+      <ul className="flex items-center h-8 -space-x-px text-sm">
+        <PageButton className="ml-0 rounded-l-lg">
+          <span className="sr-only">Previous</span>
+          <LeftArrowSvg />
+        </PageButton>
+        <PageButton>1</PageButton>
+        <PageButton>2</PageButton>
+        <PageButton state="active">3</PageButton>
+        <PageButton>4</PageButton>
+        <PageButton state="disable">5</PageButton>
+        <PageButton className='rounded-r-lg'>
             <span className="sr-only">Next</span>
             <RightArrowSvg />
-          </a>
-        </li>
+        </PageButton>
       </ul>
     </nav>
   );
