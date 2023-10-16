30f902b4ec60fd567e7fec7a74dd6d2ebb67297f
juni <woajun@naver.com>
2023-10-16 17:37:38 +0900

    add history buttons

--- app/places/History.tsx
+++ app/places/History.tsx
@@ -1,33 +1,36 @@
 import { MouseEventHandler } from 'react';
-import { useHistory, useHistoryDispatch } from '../stores/History/HistoryContext';
-import { LeftArrowSvg, XSvg } from '../svg';
+import {
+  useHistory,
+  useHistoryDispatch,
+} from '../stores/History/HistoryContext';
+import { ClipboardSvg, LeftArrowSvg, MapSvg, XSvg } from '../svg';
 
 type Props = {
   handleClick: MouseEventHandler<HTMLButtonElement>;
 };
 
-function format(date:Date) {
-    const start = new Date(date);
-    const end = new Date();
+function format(date: Date) {
+  const start = new Date(date);
+  const end = new Date();
 
-    const diff = (end.getTime() - start.getTime()) / 1000;
+  const diff = (end.getTime() - start.getTime()) / 1000;
 
-    const times = [
-      { name: '년', milliSeconds: 60 * 60 * 24 * 365 },
-      { name: '개월', milliSeconds: 60 * 60 * 24 * 30 },
-      { name: '일', milliSeconds: 60 * 60 * 24 },
-      { name: '시간', milliSeconds: 60 * 60 },
-      { name: '분', milliSeconds: 60 },
-    ];
+  const times = [
+    { name: '년', milliSeconds: 60 * 60 * 24 * 365 },
+    { name: '개월', milliSeconds: 60 * 60 * 24 * 30 },
+    { name: '일', milliSeconds: 60 * 60 * 24 },
+    { name: '시간', milliSeconds: 60 * 60 },
+    { name: '분', milliSeconds: 60 },
+  ];
 
-    for (const value of times) {
-      const betweenTime = Math.floor(diff / value.milliSeconds);
+  for (const value of times) {
+    const betweenTime = Math.floor(diff / value.milliSeconds);
 
-      if (betweenTime > 0) {
-        return `${betweenTime}${value.name} 전`;
-      }
+    if (betweenTime > 0) {
+      return `${betweenTime}${value.name} 전`;
     }
-    return '방금 전';
+  }
+  return '방금 전';
 }
 
 export default function History({ handleClick }: Props) {
@@ -35,9 +38,9 @@ export default function History({ handleClick }: Props) {
   const historyDispatch = useHistoryDispatch();
   return (
     <div className="grow overflow-y-scroll">
-      <div className='flex justify-between'>
+      <div className="flex justify-between">
         <button
-          className="text-xs border rounded-md bg-slate-300 text-white w-7 h-7 hover:bg-slate-400 active:bg-slate-300 flex justify-center items-center"
+          className="text-xs border rounded-md bg-slate-500 text-white w-7 h-7 hover:bg-slate-400 active:bg-slate-500 flex justify-center items-center"
           onClick={handleClick}
         >
           <LeftArrowSvg />
@@ -46,8 +49,8 @@ export default function History({ handleClick }: Props) {
           className="text-xs border rounded-md bg-slate-500 text-white px-2 hover:bg-slate-400 active:bg-slate-500"
           onClick={() => {
             historyDispatch!({
-              type: 'clear'
-            })
+              type: 'clear',
+            });
           }}
         >
           전체 삭제
@@ -55,25 +58,54 @@ export default function History({ handleClick }: Props) {
       </div>
       <div className="flex flex-col gap-2">
         {history.length > 0 ? (
-          history.map((e) => (
-            <div key={e.key} className="flex justify-between">
-              <div>
-                <div className="text-sm">{e.title}</div>
-                <div className="text-xs">위도 {e.position.getLat()}</div>
-                <div className="text-xs">경도 {e.position.getLng()}</div>
-              </div>
-              <div className="flex flex-col items-center shrink-0">
-                <div className="text-xs flex">
-                  {format(e.at)}
+          history
+            .map((e) => (
+              <div
+                key={e.key}
+                className="flex justify-between hover:bg-slate-100 p-1 rounded-md"
+              >
+                <div>
+                  <div className="text-sm font-semibold">{e.title}</div>
+                  <div className="text-xs">위도 {e.position.getLat()}</div>
+                  <div className="text-xs">경도 {e.position.getLng()}</div>
                 </div>
-                <div className="flex">
-                  <div>복사</div>
-                  <div>이동</div>
-                  <div><XSvg /></div>
+                <div className="flex flex-col justify-between shrink-0">
+                  <div className="text-xs flex justify-end text-slate-600">
+                    {format(e.at)}
+                  </div>
+                  <div className="flex gap-1">
+                    <button
+                      className="w-8 h-8 border-2 rounded-xl border-blue-300 text-blue-600 flex justify-center items-center hover:bg-blue-100"
+                      onClick={() => {}}
+                    >
+                      <ClipboardSvg />
+                    </button>
+                    <button
+                      className="w-8 h-8 border-2 rounded-xl border-blue-300 text-blue-600 flex justify-center items-center hover:bg-blue-100"
+                      onClick={() => {}}
+                    >
+                      <MapSvg />
+                    </button>
+                    <button
+                      className="w-8 h-8 border-2 rounded-xl border-blue-300 text-blue-600 flex justify-center items-center hover:bg-blue-100"
+                      onClick={() => {
+                        if (historyDispatch) {
+                          historyDispatch({
+                            type: 'replace',
+                            payload: history.filter(
+                              (item) => item.key !== e.key
+                            ),
+                          });
+                        }
+                      }}
+                    >
+                      <XSvg />
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          )).reverse()
+            ))
+            .reverse()
         ) : (
           <span>이전 선택 기록이 없습니다.</span>
         )}
--- app/svg/index.ts
+++ app/svg/index.ts
@@ -3,3 +3,4 @@ export { default as LeftArrowSvg } from './LeftArrowSvg';
 export { default as RightArrowSvg } from './RightArrowSvg';
 export { default as MarkerSvg } from './MarkerSvg';
 export { default as ClipboardSvg } from './ClipboardSvg';
+export { default as MapSvg } from './MapSvg';
